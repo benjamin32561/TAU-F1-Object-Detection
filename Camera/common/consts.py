82c394d7ceaecac52b5bab1056299208df386860
benjamin32561 <ben32561@gmail.com>
2022-10-25 18:59:03 +0300

    fixed dataset constant to match original dataset

--- Camera/common/consts.py
+++ Camera/common/consts.py
@@ -3,8 +3,8 @@ DATASET_SPLIT_RATIO = {"train":0.75,"val":0.15,"test":0.1}
 
 #source path consts
 SRC_PATH = "../dataset/"
-IMAGE_SUB_FOLDER = "images"
-LABELS_SUB_FOLDER = "labels"
+IMAGE_SUB_FOLDER = "img"
+LABELS_SUB_FOLDER = "ann"
 
 #source json consts
 OBJECTS = "objects"
