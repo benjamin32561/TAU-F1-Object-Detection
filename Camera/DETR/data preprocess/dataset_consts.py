f31bd552e6f337477e5a360a5b2f06dc4e324eaa
benjamin32561 <ben32561@gmail.com>
2022-11-15 12:12:42 +0200

    rebased to develop and changed dataset format to match COCO

--- Camera/DETR/data preprocess/dataset_consts.py
+++ Camera/DETR/data preprocess/dataset_consts.py
@@ -1,6 +1,8 @@
 #destination folder
 DST_PATH = "../detr_dataset/"
 DST_ANNO_PATH = DST_PATH+"annotations/" #annotations/ folder is COCO convention
+COCO_ENDING = "2017"
+COCO_JSON_START = ""
 
 #each final .json file hase common build, can be found in this file
 BASE_JSON_PATH = "base_json.json" #path to basic json path
