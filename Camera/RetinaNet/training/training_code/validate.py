806dcc909fc837aae96670efbf381a484beb0a70
benjamin32561 <ben32561@gmail.com>
2022-12-02 18:59:23 +0200

    a

--- Camera/RetinaNet/training/training_code/validate.py
+++ Camera/RetinaNet/training/training_code/validate.py
@@ -15,7 +15,7 @@ assert torch.__version__.split('.')[0] == '1'
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def Validation(model,dataloader,IoU_thresh=0.5):
-    n_images = len(dataset)
+    n_images = len(dataloader)
     for idx, data in enumerate(dataloader):
 
         img = data['img'].to(torch.float32).to(DEVICE)
