f0197ac5a9161755de6ef0e4e047c25b26cdbe8e
benjamin32561 <ben32561@gmail.com>
2023-01-01 18:45:10 +0200

    f

--- Camera/RetinaNet/training/training_code/retinanet/losses.py
+++ Camera/RetinaNet/training/training_code/retinanet/losses.py
@@ -205,7 +205,8 @@ def ValidateModel(model,dataloader,loss_fun,IoU_thresh=0.5):
         try:
             clas,reg,anch,scores,class_pred,bbx_preds = model(img)
         except Exception as e:
-            print(e)
+            print('\n',e)
+            continue
         
         annot = data['annot'].to(DEVICE)
         
