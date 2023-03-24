e80eff7495936b1b4cfc2e83503f2e7195aa128c
benjamin32561 <ben32561@gmail.com>
2023-03-24 14:38:58 +0300

    checking

--- Camera/dataset_analysis/class_analysis/fix_class_imb.py
+++ Camera/dataset_analysis/class_analysis/fix_class_imb.py
@@ -9,7 +9,7 @@ from loguru import logger
 import argparse
 from common.consts import IMAGES_SUB_FOLDER, LABELS_SUB_FOLDER, \
                         SRC_PATH, OBJECTS, CLASS_TITLE, ID
-from common.functions import GetBbxArea, GetImgHWFromJson
+from common.functions import GetBbxArea, GetImgHWFromJson, WriteJsonFiles
 
 def main():
     parser = argparse.ArgumentParser(description='My script')
@@ -71,16 +71,67 @@ def main():
     min_cass_am = class_type_cnt[min_cass]
     print(min_cass_am)
 
+    #removing min class
     del bbx_data[min_cass]
+    for key in bbx_data.keys(): #sorting each array and deleting smmallest bbx to create class balance
+        bbx_data_to_rem = sorted(bbx_data[key], key=lambda x: x["rel_area"])[:-min_cass_am]
+        for bbx in bbx_data_to_rem:
+            fie_path = bbx["fie_path"]
+            img_json_data = cf.GetDataFromJson(fie_path)
 
-    
-    print(bbx_data)
-    for key in bbx_data.keys():
-        bbx_data[key] = sorted(bbx_data[key], key=lambda x: x["rel_area"])
-    print(bbx_data)
+            old_img_bbx = img_json_data[OBJECTS]
+            img_json_data[OBJECTS] = [d for d in old_img_bbx if d.get(ID) != bbx[ID]]
+            WriteJsonFiles({fie_path:img_json_data})
     
     print("\nClass Distrebution after clean: ")
     
+    
+    #getting sub folder data
+    folders, _ = cf.GetSubFolders(SRC_PATH)
+    n_folder = 1
+
+    class_type_cnt = {}
+    bbx_data = {}
+    for src_sub_path in folders: #iterating through sub folders
+        n_folder+=1
+
+        #image and lable sorce folders
+        src_images = src_sub_path+IMAGES_SUB_FOLDER
+        src_labels = src_sub_path+LABELS_SUB_FOLDER
+
+        #loading image list and shuffeling it for random split
+        files, _ = cf.GetFilesInDir(src_images)
+
+        for original_filename in files: #iterating through images in sub folder
+
+            #load image data json file
+            json_file_path = os.path.join(src_labels, original_filename+".json")
+            img_json_data = cf.GetDataFromJson(json_file_path)
+
+            for bbx in img_json_data[OBJECTS]:
+                if bbx[CLASS_TITLE] not in class_type_cnt.keys():
+                    class_type_cnt[bbx[CLASS_TITLE]] = 1
+                    bbx_data[bbx[CLASS_TITLE]] = []
+                else:
+                    class_type_cnt[bbx[CLASS_TITLE]]+=1
+                
+                current_bbx_data = {}
+                current_bbx_data["fie_path"] = json_file_path
+                bbx_area = GetBbxArea(bbx)
+                w,h = GetImgHWFromJson(img_json_data)
+                current_bbx_data["rel_area"] = bbx_area/(w*h)
+                current_bbx_data[ID] = bbx[ID]
+                bbx_data[bbx[CLASS_TITLE]].append(current_bbx_data)
+
+    print("Class Distrebution before clean: ")
+
+    print(class_type_cnt)# create a barplot using Seaborn
+    sns.set(style="whitegrid")
+    ax = sns.barplot(x=list(class_type_cnt.keys()), y=list(class_type_cnt.values()))
+    # set the labels and title
+    ax.set(xlabel='Class Names', ylabel='Number of Objects', title='Class Object Count')
+    # display the graph
+    plt.savefig(os.path.join(args.save_at, "after.png"))
 
 if __name__ == '__main__':
     main()
