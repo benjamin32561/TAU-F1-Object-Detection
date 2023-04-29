93326080c2b927f2e21abee3dbecb3da0f462f8d
benjamin32561 <ben32561@gmail.com>
2023-04-29 19:07:02 +0300

    added an help file, now spliting code files

--- Camera/ROS/Python/camera_node/camera_node/__init__.py
+++ Camera/ROS/Python/camera_node/camera_node/__init__.py
@@ -3,48 +3,9 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-from std_msgs.msg import Header
 from cv_bridge import CvBridge
 
-import cv2
-import pyzed.sl as sl
-
-camera_settings = sl.VIDEO_SETTINGS.BRIGHTNESS
-str_camera_settings = "BRIGHTNESS"
-step_camera_settings = 1
-
-class Camera():
-    def __init__(self):
-        self.InitCamera()
-        self.print_camera_information()
-    
-    def InitCamera(self):
-        init = sl.InitParameters()
-        self.cam = sl.Camera()
-        if not self.cam.is_opened():
-            print("Opening ZED Camera...")
-        status = self.cam.open(init)
-        if status != sl.ERROR_CODE.SUCCESS:
-            print(repr(status))
-            raise Exception("Can Not Open ZED Camera")
-        
-        self.runtime = sl.RuntimeParameters()
-        self.mat = sl.Mat()
-
-    def print_camera_information(self):
-        print("Resolution: {0}, {1}.".format(round(self.cam.get_camera_information().camera_resolution.width, 2),
-                                             self.cam.get_camera_information().camera_resolution.height))
-        print("Camera FPS: {0}.".format(self.cam.get_camera_information().camera_fps))
-        print("Firmware: {0}.".format(self.cam.get_camera_information().camera_firmware_version))
-        print("Serial number: {0}.\n".format(self.cam.get_camera_information().serial_number))
-    
-    def GetImage(self):
-        img_data = None
-        err = self.cam.grab(self.runtime)
-        if err == sl.ERROR_CODE.SUCCESS:
-            self.cam.retrieve_image(self.mat, sl.VIEW.LEFT)
-            img_data = self.mat.get_data()
-        return img_data
+from camera_node.Camera import Camera
 
 class CameraNode(Node):
     def __init__(self):
